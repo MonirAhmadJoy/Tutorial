75c5353b13f8bb7d3b331981f65639febc288d28
Monir Ahmad <87666860+MonirAhmadJoy@users.noreply.github.com>
2022-05-31 18:30:32 +0600

    First commit

---
+++ ATMCaseStudy.java
@@ -0,0 +1,13 @@
+
+package ATMCaseStudy;
+import ATMCaseStudy.*;
+// ATM machine starts running from this driver function
+public class ATMCaseStudy {
+    
+ public static void main( String[] args )
+ {
+    ATM theATM = new ATM();
+    theATM.run();
+ }
+    
+}
